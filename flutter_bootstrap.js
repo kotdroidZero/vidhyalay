c6601700a64ad100be9825f32ac8a12feb70094b
Pushkar Srivastava <pushkar.srivastva8@appinventiv.com>
2025-04-17 19:21:25 +0530

    changes in inde.html

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3889628215"
+    serviceWorkerVersion: "2606135558"
   }
 });
